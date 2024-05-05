2a36678af4227ce6cdf7c9e927612ebc6321f40b
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2024-05-05 19:33:19 -0400

    fix(wasm): Use env var to copy vtk wasm binaries for development

--- trame_vtklocal/module/wasm.py
+++ trame_vtklocal/module/wasm.py
@@ -1,33 +1,42 @@
 import asyncio
 import aiohttp
 from pathlib import Path
+import os
+import shutil
 import tarfile
 
 
-async def download_file(urls, filename):
+async def download_file(url, filename):
     async with aiohttp.ClientSession() as session:
-        success = True
-        for url in urls:
-            async with session.get(url) as response:
-                data = await response.read()
-                if response.status == 404:
-                    success = False
-                else:
-                    with open(filename, "wb") as f:
-                        f.write(data)
-                        success = True
-                    break
-        if not success:
-            raise Exception("Invalid URLs " + ",".join(urls) + ". Got 404 response.")
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(filename, "wb") as f:
+                f.write(data)
 
 
-async def setup_wasm_directory(target_directory, wasm_urls):
+VTK_WASM_DIR = os.environ.get("VTK_WASM_DIR")
+
+
+async def setup_wasm_directory(target_directory, wasm_url):
     # Need to install wasm
     target_directory.mkdir(parents=True, exist_ok=True)
 
     dest_file = str((target_directory / "vtk-wasm.tgz").resolve())
     dest_folder = str(target_directory.resolve())
-    await download_file(wasm_urls, dest_file)
+
+    if VTK_WASM_DIR:
+        src_folder = Path(VTK_WASM_DIR)
+        shutil.copyfile(
+            src_folder.joinpath("vtkWasmSceneManager.mjs"),
+            target_directory.joinpath("vtkWasmSceneManager.mjs"),
+        )
+        shutil.copyfile(
+            src_folder.joinpath("vtkWasmSceneManager.wasm"),
+            target_directory.joinpath("vtkWasmSceneManager.wasm"),
+        )
+        return
+
+    await download_file(wasm_url, dest_file)
 
     # unpack
     with tarfile.open(dest_file) as tgz:
@@ -44,22 +53,18 @@ def get_wasm_info():
     version = vtk_version.GetVTKVersion()
     return (
         version,
-        (
-            # for development, start a http server in directory that has the tarball.
-            f"http://localhost:8000/vtk-wasm.{version}.tar.gz",
-            f"https://gitlab.kitware.com/api/v4/projects/13/packages/generic/vtk-wasm/{version}/vtk-wasm.{version}.tar.gz",
-        ),
+        f"https://gitlab.kitware.com/api/v4/projects/13/packages/generic/vtk-wasm/{version}/vtk-wasm.{version}.tar.gz",
     )
 
 
 def register_wasm(serve_path):
-    version, wasm_urls = get_wasm_info()
+    version, wasm_url = get_wasm_info()
     BASE_URL = f"__trame_vtklocal/wasm/{version}"
     dest_directory = Path(serve_path) / "wasm" / version
 
     if not dest_directory.exists():
         asyncio.new_event_loop().run_until_complete(
-            setup_wasm_directory(dest_directory, wasm_urls)
+            setup_wasm_directory(dest_directory, wasm_url)
         )
 
     return dict(

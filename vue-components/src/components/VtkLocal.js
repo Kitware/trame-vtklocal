122c7508313341aa5b39e7effb6d4f2d6822aae2
Corentin MUNOZ <corentin.munoz@kitware.com>
2025-04-29 16:57:04 +0200

    fix(js): fix missing function call rename from 180e1e4

--- vue-components/src/components/VtkLocal.js
+++ vue-components/src/components/VtkLocal.js
@@ -129,7 +129,7 @@ export default {
 
     function handleMessage([event]) {
       if (event.type === "state") {
-        wasmManager.pushState(event.content);
+        wasmManager.patchState(event.content);
       }
       if (event.type === "blob") {
         wasmManager.pushHash(event.hash, event.content);

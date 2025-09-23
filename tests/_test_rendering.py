70055c9bebdba6495e46acde31f2e66507e61cf9
Patrick Avery <patrick.avery@kitware.com>
2025-09-23 15:05:31 -0500

    ci: use playwright in tests
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tests/_test_rendering.py
+++ tests/_test_rendering.py
@@ -1,18 +1,12 @@
 from pathlib import Path
+
+from playwright.sync_api import Page, expect
 import pytest
-from seleniumbase import SB
-from trame_client.utils.testing import set_browser_size, baseline_comparison
 
-
-def to_baseline(example_path):
-    tpl_path = f"visual_baseline/test_rendering[{example_path}]/init/baseline.png"
-    base_path = Path(__file__).parent.parent
-    image_path = base_path / tpl_path
-
-    if not image_path.exists():
-        print(f" => Missing baseline for {image_path}")
-
-    return image_path
+from trame_client.utils.testing import (
+    assert_screenshot_matches,
+    assert_snapshot_matches,
+)
 
 
 @pytest.mark.parametrize(
@@ -22,11 +16,14 @@ def to_baseline(example_path):
         "examples/tests/rendering/flow.py",
     ],
 )
-def test_rendering(server, server_path, baseline_image):
-    with SB() as sb:
-        url = f"http://127.0.0.1:{server.port}/"
-        sb.open(url)
-        set_browser_size(sb, 600, 300)
-        sb.assert_exact_text("1", ".readyCount")
-        sb.check_window(name="init", level=3)
-        baseline_comparison(sb, to_baseline(server_path), 0.1)
+def test_rendering(server, server_path, page: Page, ref_dir: Path):
+    url = f"http://127.0.0.1:{server.port}/"
+    page.goto(url)
+
+    page.set_viewport_size({"width": 600, "height": 300})
+    expect(page.locator(".readyCount")).to_have_text("1")
+
+    base_name = Path(server_path).stem
+    name = f"test_rendering_{base_name}"
+    assert_snapshot_matches(page, ref_dir, name)
+    assert_screenshot_matches(page, ref_dir, name, threshold=0.1)

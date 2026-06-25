c5a136a4b7a6dd4ce537cca0426f935e97e5ffef
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-06-25 15:57:08 -0600

    docs(example): add camera jump

---
+++ examples/validation/camera_jump.py
@@ -0,0 +1,175 @@
+import asyncio
+
+# Required for vtk factory
+import vtkmodules.vtkRenderingOpenGL2  # noqa
+from trame.app import TrameApp, asynchronous
+from trame.decorators import change
+from trame.ui.html import DivLayout
+from vtkmodules.vtkFiltersSources import vtkConeSource
+from vtkmodules.vtkInteractionStyle import vtkInteractorStyleSwitch  # noqa
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderer,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+)
+
+from trame.widgets import client, html
+from trame_vtklocal.widgets import vtklocal
+
+CLIENT_TYPE = "vue3"
+
+# -----------------------------------------------------------------------------
+# VTK pipeline
+# -----------------------------------------------------------------------------
+
+
+def create_vtk_pipeline():
+    renderer = vtkRenderer()
+    renderWindow = vtkRenderWindow()
+    renderWindow.AddRenderer(renderer)
+
+    renderWindowInteractor = vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+    renderWindowInteractor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
+
+    cone = vtkConeSource()
+
+    mapper = vtkPolyDataMapper()
+    mapper.SetInputConnection(cone.GetOutputPort())
+
+    actor = vtkActor()
+    actor.SetMapper(mapper)
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(0.1, 0.2, 0.4)
+    renderer.ResetCamera()
+
+    return renderWindow, cone, actor
+
+
+class DemoApp(TrameApp):
+    def __init__(self, server=None):
+        # dispose after unmount should only work with vue3
+        super().__init__(server, client_type=CLIENT_TYPE)
+        self.delta = 1
+        self.server.cli.add_argument(
+            "--mode",
+            default="wasm32",
+        )
+        self.server.cli.add_argument(
+            "--exec",
+            default="sync",
+        )
+        self.server.cli.add_argument(
+            "--rendering",
+            default="webgl",
+        )
+        args, _ = self.server.cli.parse_known_args()
+        self.state.mode = args.mode
+        self.state.exec = args.exec
+        self.state.rendering = args.rendering
+
+        self.render_window, self.cone, self.actor = create_vtk_pipeline()
+        self._build_ui()
+
+    def reset_camera(self):
+        self.ctx.view.reset_camera()
+
+    @change("resolution")
+    def on_resolution_change(self, resolution, **_):
+        self.cone.SetResolution(int(resolution))
+        self.ctx.view.update_throttle(
+            resolution=resolution
+        )  # provide custom content on update
+
+    @change("play")
+    def on_play_change(self, play, **_):
+        if play:
+            asynchronous.create_task(self._animate())
+
+    async def _animate(self):
+        while self.state.play:
+            await asyncio.sleep(0.01)
+
+            resolution = self.cone.resolution
+
+            if resolution > 60:
+                self.delta = -1
+            if resolution < 4:
+                self.delta = +1
+
+            self.on_resolution_change(resolution + self.delta)
+
+    def _build_ui(self):
+        with DivLayout(self.server) as self.ui:
+            client.Style("body { margin: 0; }")
+            with html.Div(
+                style="position: absolute; left: 0; top: 0; width: 100vw; height: 100vh;",
+                v_if=("mounted", True),
+            ):
+                self.html_view = vtklocal.LocalView(
+                    self.render_window,
+                    ctx_name="view",
+                    throttle_rate=20,
+                    config=("{ mode, rendering, exec }",),
+                )
+            with html.Div(
+                style="""
+                    position: absolute;
+                    left: 1rem;
+                    right: 1rem;
+                    top: 1rem;
+                    display: flex;
+                    padding: 10px;
+                    background: white;
+                    gap: 15px;
+                """
+            ):
+                html.Div("Resolution")
+                html.Input(
+                    type="range",
+                    v_model=("resolution", 6),
+                    min=3,
+                    max=60,
+                    step=1,
+                )
+
+                with html.Select(v_model=("mode", "wasm32")):
+                    html.Option("wasm32")
+                    html.Option("wasm64")
+                with html.Select(v_model=("exec", "sync")):
+                    html.Option("sync")
+                    html.Option("async")
+                with html.Select(v_model=("rendering", "webgl")):
+                    html.Option("webgl")
+                    html.Option("webgpu")
+
+                html.Div("Animate")
+                html.Input(
+                    type="checkbox",
+                    v_model=("play", False),
+                )
+                html.Div("Component mounted")
+                html.Input(
+                    type="checkbox",
+                    v_model=("mounted", True),
+                )
+                html.Button("Dispose Runtime", click=self.ctx.view.dispose_wasm_runtime)
+                html.Button(
+                    "Dispose Session", click=self.ctx.view.dispose_remote_session
+                )
+                html.Button(
+                    "Reset Camera",
+                    click=self.reset_camera,
+                )
+
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    app = DemoApp()
+    app.server.start()
